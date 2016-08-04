944ccec8b6be1389d18f6cd9708a9046af673675
Aaron L. Zeng <a2z@mit.edu>
2016-08-04 01:07:18 -0400

    Remove unused core_extended

--- src/sol_023.ml
+++ src/sol_023.ml
@@ -1,5 +1,4 @@
 open Core.Std
-open Core_extended.Std
 
 module M = struct
   let problem_number = 23
@@ -22,53 +21,29 @@ module M = struct
     )
 
   let main () =
-    match Sys.getenv "USE_BITARRAY" with
-    | Some _ ->
-      let can_sum = Bitarray.create (limit + 1) in
-      let rec iter outer inner =
-        match outer, inner with
-        | [], []        -> ()
-        | [], _         -> assert false
-        | (_ :: tl), [] -> iter tl tl
-        | (x :: xs), (y :: ys) ->
-          if x + y > limit
-          then iter xs xs
-          else (
-            Bitarray.set can_sum (x + y) true;
-            iter outer ys
-          )
-      in
-      let abundant_numbers = force abundant_numbers in
-      iter abundant_numbers abundant_numbers;
-      let sum = ref 0 in
-      for i = 0 to limit do
-        if not (Bitarray.get can_sum i)
-        then sum := !sum + i
-      done;
-      printf "%d\n" !sum
-    | None ->
-      let can_sum = Array.create false ~len:(limit + 1) in
-      let rec iter outer inner =
-        match outer, inner with
-        | [], []        -> ()
-        | [], _         -> assert false
-        | (_ :: tl), [] -> iter tl tl
-        | (x :: xs), (y :: ys) ->
-          if x + y > limit
-          then iter xs xs
-          else (
-            can_sum.(x + y) <- true;
-            iter outer ys
-          )
-      in
-      let abundant_numbers = force abundant_numbers in
-      iter abundant_numbers abundant_numbers;
-      let sum = ref 0 in
-      for i = 0 to limit do
-        if not can_sum.(i)
-        then sum := !sum + i
-      done;
-      printf "%d\n" !sum
+    let can_sum = Array.create false ~len:(limit + 1) in
+    let rec iter outer inner =
+      match outer, inner with
+      | [], [] -> ()
+      | [], _  ->
+        failwiths "unexpected state" (outer, inner)
+          [%sexp_of: int list * int list]
+      | (_ :: tl), [] -> iter tl tl
+      | (x :: xs), (y :: ys) ->
+        if x + y > limit
+        then iter xs xs
+        else (
+          can_sum.(x + y) <- true;
+          iter outer ys)
+    in
+    let abundant_numbers = force abundant_numbers in
+    iter abundant_numbers abundant_numbers;
+    let sum = ref 0 in
+    for i = 0 to limit do
+      if not can_sum.(i)
+      then sum := !sum + i
+    done;
+    printf "%d\n" !sum
 end
 
 include Solution.Make(M)

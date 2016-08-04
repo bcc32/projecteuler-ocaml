a2745ea40eb8b2b6d2eb793c538ef080cb04bbb3
Aaron L. Zeng <a2z@mit.edu>
2016-08-03 23:39:41 -0400

    Solve problem 47

---
+++ src/sol_047.ml
@@ -0,0 +1,19 @@
+open Core.Std
+
+module M = struct
+  let problem_number = 47
+
+  let main () =
+    let f n = n |> Euler.prime_factor |> List.length |> Int.equal 4 in
+    let numbers =
+      Euler.natural_numbers ~init:1 ()
+      |> Sequence.unfold_with ~init:0 ~f:(fun s n ->
+        let consec_ok = if f n then s + 1 else 0 in
+        Yield ((n, consec_ok), consec_ok))
+    in
+    let last = Sequence.find_exn numbers ~f:(fun (_, c) -> c = 4) in
+    let ans = fst last - 3 in
+    printf "%d\n" ans
+end
+
+include Solution.Make(M)

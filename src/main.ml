4f7d477846e4964f376b08637c625b71697a8ee1
Aaron L. Zeng <a2z@mit.edu>
2016-08-06 15:49:39 -0400

    Solve problem 43

--- src/main.ml
+++ src/main.ml
@@ -38,6 +38,7 @@ let command =
     ; "39"    , Sol_039     .command
     ; "41"    , Sol_041     .command
     ; "42"    , Sol_042     .command
+    ; "43"    , Sol_043     .command
     ; "47"    , Sol_047     .command
     ; "48"    , Sol_048     .command
     ; "55"    , Sol_055     .command

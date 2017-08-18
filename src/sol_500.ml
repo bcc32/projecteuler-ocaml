f9d982b4dcc96044533c7d2c4b58a294e4547461
Aaron L. Zeng <a2z@mit.edu>
2017-08-18 02:43:57 +0200

    Sieve solution for 500
    "Inspired by" matteose's solution at
    https://projecteuler.net/thread=500;page=2#195344.

--- src/sol_500.ml
+++ src/sol_500.ml
@@ -10,21 +10,30 @@ module M = struct
   let upper_bound = 7_400_000
 
   let main () =
-    let queue =
-      Euler.prime_sieve upper_bound
-      |> Array.filter_mapi ~f:(fun i p -> Option.some_if p i)
-      |> Heap.of_array ~cmp:Int.compare
+    let sieve = Array.create false ~len:upper_bound in
+    let rec loop i ac count =
+      if count >= 500_500
+      then ac
+      else (
+        if not sieve.(i)
+        then (
+          let count = count + 1 in
+          let ac = ac * i mod modulo in
+          let rec mark j =
+            if j < upper_bound
+            then (
+              sieve.(j) <- true;
+              mark (j + i))
+          in
+          mark (2 * i);
+          if i * i < upper_bound
+          then (sieve.(i * i) <- false);
+          loop (i + 1) ac count)
+        else (
+          loop (i + 1) ac count))
     in
-    let number = ref 1 in
-    for _ = 1 to 500_500 do
-      let factor = Heap.pop_exn queue in
-      number := !number * factor mod modulo;
-      let factor = factor * factor in
-      if factor < upper_bound
-      then (Heap.add queue factor)
-    done;
-    !number
+    loop 2 1 0
     |> printf "%d\n"
-    (* 35407281, 500ms *)
+    (* 35407281, 325ms *)
 end
 include Solution.Make(M)

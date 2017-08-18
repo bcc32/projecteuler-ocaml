4c1b9208e3abe78f94d845d9a2281e5924ef3bcd
Aaron L. Zeng <a2z@mit.edu>
2017-08-18 02:23:59 +0200

    Optimize performance of 500
    Significant simplifications can be made by avoiding extra
    book-keeping (and floating-point operations). We know the upper bound on
    the factors that might make up our number is small (less than
    `p_{500_500}`). We use an optimized prime sieve to populate our initial
    heap and proceed from there.

--- src/sol_500.ml
+++ src/sol_500.ml
@@ -2,65 +2,29 @@ open Core
 
 let modulo = 500_500_507
 
-module Factor = struct
-  module T = struct
-    type t =
-      { prime    : int
-      ; exponent : int
-      ; value    : int          (* modulo [modulo] *)
-      ; log      : float
-      }
-    [@@deriving sexp]
-
-    let compare = Comparable.lift Float.compare ~f:(fun t -> t.log)
-  end
-
-  include T
-  include Comparable.Make(T)
-
-  let of_prime prime =
-    if not (Euler.Int.is_prime prime)
-    then (raise_s [%message "not a prime" (prime : int)]);
-    { prime
-    ; exponent = 1
-    ; value    = prime mod modulo
-    ; log      = Float.log (Float.of_int prime)
-    }
-
-  let next t =
-    { prime    = t.prime
-    ; exponent = 2 * t.exponent
-    ; value    = t.value * t.value mod modulo
-    ; log      = 2. *. t.log
-    }
-end
-
 module M = struct
   let problem_number = 500
 
   let power_of_two_divisors k =
-    let queue = Heap.create ~cmp:Factor.compare () in
-    Heap.add queue (Factor.of_prime 2);
+    let queue = Heap.create ~cmp:Int.compare () in
+    (* We know [PrimePi[8_000_000] >= 500_500] so this includes all first
+       500,000 primes, which is the upper bound on the primes used. *)
+    Euler.prime_sieve 8_000_000
+    |> Array.iteri ~f:(fun i p ->
+      if p
+      then (Heap.add queue i)
+    );
     let number = ref 1 in
-    for i = 1 to k do
-      if i mod 1000 = 0
-      then (
-        Debug.eprintf "i %d" i
-      );
+    for _ = 1 to k do
       let factor = Heap.pop_exn queue in
-      number := !number * factor.value mod modulo;
-      Heap.add queue (Factor.next factor);
-      if factor.exponent = 1
-      then (
-        let prime = Euler.Int.next_prime factor.prime in
-        Heap.add queue (Factor.of_prime prime)
-      )
+      number := !number * factor mod modulo;
+      Heap.add queue (factor * factor)
     done;
     !number
 
   let main () =
     power_of_two_divisors 500_500
     |> printf "%d\n"
-    (* 35407281, 48s *)
+    (* 35407281, 672ms *)
 end
 include Solution.Make(M)

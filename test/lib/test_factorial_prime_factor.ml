4372c0b12ccaa9381aaae29362c85647c65cd31e
Aaron L. Zeng <a2z@mit.edu>
2017-10-07 21:43:50 -0400

    Add tests for factorial_prime_factor

---
+++ test/lib/test_factorial_prime_factor.ml
@@ -0,0 +1,17 @@
+open! Core
+open Bignum.Std
+
+let%test_unit "factorial_prime_factor" =
+  let gen = Quickcheck.Generator.small_positive_int in
+  Quickcheck.test gen
+    ~sexp_of:[%sexp_of: int]
+    ~f:(fun n ->
+      let expect =
+        n
+        |> Bigint.of_int
+        |> Euler.Bigint.factorial
+        |> Euler.Bigint.prime_factor
+        |> List.map ~f:(Tuple2.map_fst ~f:Bigint.to_int_exn)
+      in
+      [%test_result: (int * int) list] ~expect
+        (Euler.factorial_prime_factor n))

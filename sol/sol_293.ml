0f4e34c42064d682411e6e7a210e158cfa227ffa
Aaron L. Zeng <me@bcc32.com>
2017-12-23 14:16:03 -0600

    Solve problem 293

--- sol/sol_293.ml
+++ sol/sol_293.ml
@@ -7,6 +7,8 @@ module M = struct
   let is_prime = lazy (Number_theory.prime_sieve 100_000)
 
   let is_admissible n =
+    if n % 1_000_000 = 0
+    then (Debug.eprintf !"%{Int#hum}" n);
     let rec start n i =
       if n = 1
       then true
@@ -24,13 +26,26 @@ module M = struct
     start n 2
   ;;
 
+  let pseudo_fortunate n =
+    let rec loop i =
+      if Number_theory.Int.is_prime (n + i)
+      then i
+      else (loop (i + 1))
+    in
+    loop 2
+  ;;
+
   let main () =
-    Number_theory.Int.natural_numbers ~init:1 ()
+    Number_theory.Int.natural_numbers ~init:2 ()
     |> Sequence.take_while ~f:(fun x -> x < 1_000_000_000)
     |> Sequence.filter ~f:is_admissible
-    |> Sequence.length
+    |> Sequence.map ~f:pseudo_fortunate
+    |> Sequence.fold ~init:Int.Set.empty ~f:Set.add
+    |> Set.sum (module Int) ~f:Fn.id
     |> printf "%d\n"
   ;;
+  (* 2209
+     2.3m *)
 end
 
 include Solution.Make(M)

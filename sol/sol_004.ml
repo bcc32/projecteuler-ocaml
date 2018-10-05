010f231e07d309e7f0ee7e588ded63b78c7ced24
Aaron L. Zeng <me@bcc32.com>
2018-10-05 17:38:05 -0400

    Replace digit list with string in problem 4
    Approximately 30% speedup

--- sol/sol_004.ml
+++ sol/sol_004.ml
@@ -3,13 +3,14 @@ open! Import
 
 module M = struct
   let problem = `Number 4
+  let is_palindrome s = String.( = ) s (String.rev s)
 
   let main () =
     let ans = ref 0 in
     for i = 100 to 999 do
       for j = 100 to 999 do
-        let digits = i * j |> Number_theory.Int.digits_of_int in
-        if Sequences.is_palindrome ~equal:Int.equal digits && i * j > !ans then ans := i * j
+        let digits = i * j |> Int.to_string in
+        if is_palindrome digits && i * j > !ans then ans := i * j
       done
     done;
     printf "%d\n" !ans

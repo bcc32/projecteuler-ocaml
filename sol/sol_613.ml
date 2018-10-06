6faf8872280a49eeeb6ba7ec8432f91fca31554d
Aaron L. Zeng <me@bcc32.com>
2018-10-06 02:23:44 -0400

    Problem 613: update style

--- sol/sol_613.ml
+++ sol/sol_613.ml
@@ -3,23 +3,16 @@ open! Import
 
 module M = struct
   let problem = `Number 613
-  let tau = 2. *. Float.pi
-
-  (* x/40 + y/30 = 1 *)
 
   let main () =
     let open Float.O in
     Numerics.Float.integrate () ~low:0. ~high:40. ~intervals:32_000 ~f:(fun x ->
-      Numerics.Float.integrate
-        ()
-        ~low:0.
-        ~high:(30. - (x * 0.75))
-        ~intervals:32_000
-        ~f:(fun y ->
-          let to_southeast = Float.atan2 (-y) (40. - x) in
-          let to_northwest = Float.atan2 (30. - y) (-x) in
-          (to_northwest - to_southeast) / tau))
-    / (30. * 40. / 2.)
+      let max_y = 30. - (x * 0.75) in
+      Numerics.Float.integrate () ~low:0. ~high:max_y ~intervals:32_000 ~f:(fun y ->
+        let to_southeast = Float.atan2 (-y) (40. - x) in
+        let to_northwest = Float.atan2 (30. - y) (-x) in
+        to_northwest - to_southeast))
+    / (Float.pi * 30. * 40.)
     |> printf "%.10f\n"
   ;;
 

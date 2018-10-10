d66d714bd7d26d4421ff1f7d8297fedb1546d67e
Aaron L. Zeng <me@bcc32.com>
2018-10-10 15:11:51 -0400

    Avoid constructing bigint strings in 25

--- sol/sol_025.ml
+++ sol/sol_025.ml
@@ -3,17 +3,17 @@ open! Import
 
 module M = struct
   let problem = Number 25
+  let threshold = Bigint.(pow (of_int 10) (of_int 999))
 
   let main () =
-    Sequence.findi Number_theory.Bigint.fibonacci ~f:(fun _ f ->
-      String.length (Bigint.to_string f) >= 1000)
+    Sequence.findi Number_theory.Bigint.fibonacci ~f:(fun _ f -> Bigint.(f >= threshold))
     |> uw
     |> fst
     |> printf "%d\n"
   ;;
 
   (* 4782
-     37ms *)
+     1.692ms *)
 end
 
 include Solution.Make (M)

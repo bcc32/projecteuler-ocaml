610e49dec09aed1c9787ec1cf2602f22382ad3da
Aaron L. Zeng <me@bcc32.com>
2018-10-10 14:52:37 -0400

    Compile euler_data lib under jenga

---
+++ data/euler_data.ml
@@ -0,0 +1,7 @@
+module Problem_008 = Problem_008
+module Problem_011 = Problem_011
+module Problem_013 = Problem_013
+module Problem_018 = Problem_018
+module Problem_022 = Problem_022
+module Problem_042 = Problem_042
+module Problem_067 = Problem_067

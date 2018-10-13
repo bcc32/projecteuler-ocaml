d519d75e5832a9f1083d9488e993dda2c1f9341b
Aaron L. Zeng <me@bcc32.com>
2018-10-13 14:28:18 -0400

    47: simplify implementation
    - Don't eagerly count consecutives.
    - Use memoized stream of prime factor counts
    
    Approximately 20% speedup.

--- sol/sol_047.ml
+++ sol/sol_047.ml
@@ -5,20 +5,32 @@ module M = struct
   let problem = Number 47
 
   let main () =
-    let f n = List.length (Number_theory.Int.prime_factor n) = 4 in
-    let numbers =
-      Number_theory.Int.natural_numbers ~init:1 ()
-      |> Sequence.unfold_with ~init:0 ~f:(fun s n ->
-        let consec_ok = if f n then s + 1 else 0 in
-        Yield ((n, consec_ok), consec_ok))
+    let distinct_prime_factors =
+      Number_theory.Int.natural_numbers ~init:2 ()
+      |> Sequence.map ~f:(fun n ->
+        if debug then Debug.eprintf "factoring %d" n;
+        n, Number_theory.Int.prime_factor n |> List.length)
     in
-    let last = Sequence.find_exn numbers ~f:(fun (_, c) -> c = 4) in
-    let ans = fst last - 3 in
-    printf "%d\n" ans
+    let each_consecutive_4 =
+      let a = distinct_prime_factors |> Sequence.memoize in
+      let b = a |> Sequence.tl_eagerly_exn in
+      let c = b |> Sequence.tl_eagerly_exn in
+      let d = c |> Sequence.tl_eagerly_exn in
+      (* TODO Replace this with a zip-based applicative. *)
+      Sequence.zip (Sequence.zip a b) (Sequence.zip c d)
+      |> Sequence.map ~f:(fun ((a, b), (c, d)) -> a, b, c, d)
+    in
+    each_consecutive_4
+    |> Sequence.find_map ~f:(function (a, apf), (_, bpf), (_, cpf), (_, dpf) ->
+      (* Ideally, we'd like to skip multiple 4-tuples immediately if, e.g.,
+         [apf <> 4]. *)
+      if apf = 4 && bpf = 4 && cpf = 4 && dpf = 4 then Some a else None)
+    |> uw
+    |> printf "%d\n"
   ;;
 
   (* 134043
-     261ms *)
+     210.016ms *)
 end
 
 include Solution.Make (M)

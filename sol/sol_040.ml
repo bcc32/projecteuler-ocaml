8b7af3cd1b7ff43928a20fb70c8662a98d3ae160
Aaron L. Zeng <me@bcc32.com>
2018-10-13 13:22:41 -0400

    40: process sequence online without memoizing
    Approximately 2.3x speedup.

--- sol/sol_040.ml
+++ sol/sol_040.ml
@@ -2,24 +2,26 @@ open! Core
 open! Import
 
 let champernowne's_constant =
-  Sequence.memoize
-    (let open Sequence.Let_syntax in
-     let%bind nat = Number_theory.Int.natural_numbers () ~init:1 in
-     Number_theory.Int.digits_of_int nat |> Sequence.of_list)
+  let open Sequence.Let_syntax in
+  let%bind nat = Number_theory.Int.natural_numbers () ~init:1 in
+  Number_theory.Int.digits_of_int nat |> Sequence.of_list
 ;;
 
 module M = struct
   let problem = Number 40
+  let limit = 1_000_000
   let indices = [ 1; 10; 100; 1_000; 10_000; 100_000; 1_000_000 ]
 
   let main () =
-    List.fold indices ~init:1 ~f:(fun ac i ->
-      ac * Sequence.nth_exn champernowne's_constant (i - 1))
+    champernowne's_constant
+    |> Fn.flip Sequence.take limit
+    |> Sequence.foldi ~init:1 ~f:(fun i ac digit ->
+      if List.mem indices (i + 1) ~equal:Int.equal then ac * digit else ac)
     |> printf "%d\n"
   ;;
 
   (* 210
-     262.095ms *)
+     112.283ms *)
 end
 
 include Solution.Make (M)

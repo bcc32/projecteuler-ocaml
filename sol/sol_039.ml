5c45b0ae795c189774bc2087a5261379eaaa3d85
Aaron L. Zeng <me@bcc32.com>
2018-10-13 13:09:45 -0400

    39: avoid some Sequence overhead
    Approximately 2.5x speedup.

--- sol/sol_039.ml
+++ sol/sol_039.ml
@@ -7,22 +7,22 @@ module M = struct
   let main () =
     Sequence.range ~stop:`inclusive 12 1000
     |> Sequence.map ~f:(fun p ->
-      let solutions =
-        Sequence.range 5 (p / 2)
-        |> Sequence.sum
-             (module Int)
-             ~f:(fun c ->
-               Sequence.range ((p - c) / 2) c
-               |> Sequence.count ~f:(fun b ->
-                 let a = p - c - b in
-                 Geometry.is_pythagorean_triple a b c))
-      in
-      p, solutions)
+      let solutions = ref 0 in
+      for c = 5 to (p / 2) - 1 do
+        for b = (p - c) / 2 to c - 1 do
+          let a = p - c - b in
+          if Geometry.is_pythagorean_triple a b c then incr solutions
+        done
+      done;
+      p, !solutions)
     |> Sequence.max_elt ~compare:(fun (_, a) (_, b) -> Int.compare a b)
     |> uw
     |> fst
     |> printf "%d\n"
   ;;
+
+  (* 840
+     40.293ms *)
 end
 
 include Solution.Make (M)

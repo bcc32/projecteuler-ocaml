fab61e9deae576a9ad3c1f1cb1c479153d6403e7
Aaron L. Zeng <me@bcc32.com>
2018-10-13 14:15:40 -0400

    45: factor out merge logic, remove debug

--- sol/sol_045.ml
+++ sol/sol_045.ml
@@ -5,33 +5,19 @@ module M = struct
   let problem = Number 45
   let prev = 40755
 
-  let select_dups (s : ('a, 'a) Sequence.Merge_with_duplicates_element.t Sequence.t) =
-    Sequence.filter_map s ~f:(function
+  let select_dups xs ys =
+    Sequence.merge_with_duplicates xs ys ~cmp:Int.compare
+    |> Sequence.filter_map ~f:(function
       | Left _ | Right _ -> None
       | Both (x, _) -> Some x)
   ;;
 
   let main () =
     let map_nats ~f = Number_theory.Int.natural_numbers () ~init:1 |> Sequence.map ~f in
-    let triangle =
-      map_nats ~f:(fun n ->
-        if debug then Debug.eprintf "tri %d" n;
-        n * (n + 1) / 2)
-    in
-    let pentagonal =
-      map_nats ~f:(fun n ->
-        if debug then Debug.eprintf "pent %d" n;
-        n * ((3 * n) - 1) / 2)
-    in
-    let hexagonal =
-      map_nats ~f:(fun n ->
-        if debug then Debug.eprintf "hex %d" n;
-        n * ((2 * n) - 1))
-    in
-    Sequence.merge_with_duplicates triangle pentagonal ~cmp:Int.compare
-    |> select_dups
-    |> Sequence.merge_with_duplicates hexagonal ~cmp:Int.compare
-    |> select_dups
+    let triangle = map_nats ~f:(fun n -> n * (n + 1) / 2) in
+    let pentagonal = map_nats ~f:(fun n -> n * ((3 * n) - 1) / 2) in
+    let hexagonal = map_nats ~f:(fun n -> n * ((2 * n) - 1)) in
+    select_dups (select_dups triangle pentagonal) hexagonal
     |> Sequence.drop_while ~f:(fun x -> x <= prev)
     |> Sequence.hd_exn
     |> printf "%d\n"

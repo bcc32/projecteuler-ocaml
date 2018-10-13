86f4d5f08a8fcbee5df0b3b482630769ac015a81
Aaron L. Zeng <me@bcc32.com>
2018-10-13 13:44:21 -0400

    43: permute digits in-place
    Approximately 2x speedup.

--- sol/sol_043.ml
+++ sol/sol_043.ml
@@ -7,7 +7,7 @@ module M = struct
   let check_digits d =
     let is_div d1 d2 d3 p = ((100 * d1) + (10 * d2) + d3) mod p = 0 in
     match d with
-    | [ _d1; d2; d3; d4; d5; d6; d7; d8; d9; d10 ] ->
+    | [| _d1; d2; d3; d4; d5; d6; d7; d8; d9; d10 |] ->
       is_div d2 d3 d4 2
       && is_div d3 d4 d5 3
       && is_div d4 d5 d6 5
@@ -19,17 +19,19 @@ module M = struct
   ;;
 
   let main () =
-    List.range 0 10
-    |> Sequences.permutations ~compare:Int.compare
-    |> Sequence.filter ~f:check_digits
-    |> Sequence.sum
-         (module Int)
-         ~f:(fun d -> Number_theory.Int.int_of_digits (Sequence.of_list d))
-    |> printf "%d\n"
+    let digits = List.range 0 10 |> Array.of_list in
+    let sum = ref 0 in
+    let rec loop () =
+      if check_digits digits
+      then sum := !sum + Number_theory.Int.int_of_digits (Array.to_sequence digits);
+      if Sequences.next_permutation_inplace digits ~compare:Int.compare then loop ()
+    in
+    loop ();
+    printf "%d\n" !sum
   ;;
 
   (* 16695334890
-     1.42737s *)
+     694.092ms *)
 end
 
 include Solution.Make (M)

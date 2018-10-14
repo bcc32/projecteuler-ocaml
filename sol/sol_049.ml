e1456b9e5736acd4da385511e263e4687ce29592
Aaron L. Zeng <me@bcc32.com>
2018-10-14 01:08:23 -0400

    49: refactor, cleanup

--- sol/sol_049.ml
+++ sol/sol_049.ml
@@ -3,44 +3,45 @@ open! Import
 
 module M = struct
   let problem = Number 49
+  let example_x = 1487
+
+  let mem_binary haystack needle =
+    Array.binary_search haystack ~compare:Int.compare `First_equal_to needle
+    |> Option.is_some
+  ;;
 
   let main () =
     let is_prime = Number_theory.prime_sieve 10_000 in
-    let checked = Array.create false ~len:10_000 in
+    let visited = Array.create false ~len:10_000 in
     try
       for a = 1000 to 9999 do
-        if not checked.(a)
-        then
-          if is_prime.(a)
-          then (
-            let numbers =
-              a
-              |> Number_theory.Int.digits_of_int
-              |> Sequences.permutations ~compare:Int.compare
-              |> Sequence.map ~f:(fun digits ->
-                digits |> Sequence.of_list |> Number_theory.Int.int_of_digits)
-              |> Sequence.filter ~f:(Array.get is_prime)
-              |> Sequence.to_array
-            in
-            Array.sort numbers ~compare:Int.compare;
-            for i = 0 to Array.length numbers - 1 do
-              checked.(numbers.(i)) <- true;
-              for j = i + 1 to Array.length numbers - 1 do
-                let x = numbers.(i) in
-                let y = numbers.(j) in
-                if Array.binary_search
-                     numbers
-                     ~compare:Int.compare
-                     `First_equal_to
-                     (y + (y - x))
-                   |> Option.is_some
-                && x >= 1000
-                && x <> 1487
+        if not visited.(a) && is_prime.(a)
+        then (
+          let prime_permutations =
+            a
+            |> Number_theory.Int.digits_of_int
+            |> Sequences.permutations ~compare:Int.compare
+            |> Sequence.map ~f:(fun digits ->
+              digits |> Sequence.of_list |> Number_theory.Int.int_of_digits)
+            |> Sequence.filter ~f:(Array.get is_prime)
+            |> Sequence.to_array
+          in
+          Array.sort prime_permutations ~compare:Int.compare;
+          for i = 0 to Array.length prime_permutations - 1 do
+            (* skip prime_permutations with leading zeros *)
+            if prime_permutations.(i) >= 1000
+            then (
+              visited.(prime_permutations.(i)) <- true;
+              for j = i + 1 to Array.length prime_permutations - 1 do
+                let x = prime_permutations.(i) in
+                let y = prime_permutations.(j) in
+                let z = y + (y - x) in
+                if mem_binary prime_permutations z && x <> example_x
                 then (
-                  printf "%d%d%d\n" x y (y + (y - x));
+                  printf "%d%d%d\n" x y z;
                   raise Exit)
-              done
-            done)
+              done)
+          done)
       done
     with
     | Exit -> ()

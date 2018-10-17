d9a8dec0d904de76dcaa1614b5937b813355736a
Aaron L. Zeng <me@bcc32.com>
2018-10-16 20:30:17 -0400

    24: directly select nth permutation
    From ~160ms to <0.1ms

--- sol/sol_024.ml
+++ sol/sol_024.ml
@@ -3,15 +3,23 @@ open! Import
 
 module M = struct
   let problem = Number 24
+  let elements = 10
+  let index = 999_999
 
   let main () =
-    let sequence = Array.init 10 ~f:Fn.id in
-    for _ = 1 to 999_999 do
-      assert (Sequences.next_permutation_inplace sequence ~compare:Int.compare)
-    done;
-    sequence
-    |> Array.map ~f:Int.to_string
-    |> String.concat_array ~sep:""
+    let rec loop i index candidates =
+      if Set.is_empty candidates
+      then []
+      else (
+        let elements_after_ith = elements - i - 1 in
+        let permutations = Number_theory.Int.factorial elements_after_ith in
+        let nth_at_i = index / permutations in
+        let x = Set.nth candidates nth_at_i |> Option.value_exn ~here:[%here] in
+        x :: loop (i + 1) (index % permutations) (Set.remove candidates x))
+    in
+    loop 0 index (Int.Set.of_increasing_iterator_unchecked ~len:elements ~f:Fn.id)
+    |> List.map ~f:Int.to_string
+    |> String.concat
     |> printf "%s\n"
   ;;
 end

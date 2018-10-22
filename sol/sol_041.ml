bab60dc5f9cee1c4ddeab96a1bddf90d01c0c3e8
Aaron L. Zeng <me@bcc32.com>
2018-10-22 18:06:05 -0400

    41: Replace silly use of Sequence

--- sol/sol_041.ml
+++ sol/sol_041.ml
@@ -5,21 +5,21 @@ module M = struct
   let problem = Number 41
 
   let main () =
-    Sequence.range ~stride:(-1) 9 0
-    |> Sequence.find_map ~f:(fun n ->
-      let digits = Array.init n ~f:(fun i -> n - i) in
-      let rec loop () =
-        let n =
-          Array.to_sequence_mutable digits |> Number_theory.Int.int_of_digits
+    with_return (fun { return } ->
+      for n = 9 downto 0 do
+        let digits = Array.init n ~f:(fun i -> n - i) in
+        let rec loop () =
+          let n =
+            Array.to_sequence_mutable digits |> Number_theory.Int.int_of_digits
+          in
+          if Number_theory.Int.is_prime n
+          then return n
+          else if Sequences.next_permutation_inplace digits ~compare:Int.descending
+          then loop ()
         in
-        if Number_theory.Int.is_prime n
-        then Some n
-        else if Sequences.next_permutation_inplace digits ~compare:Int.descending
-        then loop ()
-        else None
-      in
-      loop ())
-    |> Option.value_exn
+        loop ()
+      done;
+      assert false)
     |> printf "%d\n"
   ;;
 

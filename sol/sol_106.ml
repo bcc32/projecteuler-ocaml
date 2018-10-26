4e569ebf6e10265262c85ebafbff14f072c2f66b
Aaron L. Zeng <me@bcc32.com>
2018-10-26 05:49:09 -0400

    Cleanup 106 and add comments

--- sol/sol_106.ml
+++ sol/sol_106.ml
@@ -1,13 +1,22 @@
 open! Core
 open! Import
 
-let number_of_subset_pair_unions n k = Number_theory.Int.binomial n (2 * k)
+(* In order for a pair of sets to satisfy rule 2 but not rule 2, they need to be
+   of the same size, since rule 2 implies that the sums of the sets are not
+   equal.
 
-exception Went_negative
+   For any pair of disjoint equal-size sets (L, R), where WLOG min(L) < min(R),
+   if L is "dominated" by R, then we know sum(L) < sum(R).  I.e., if l_1 < r_1,
+   l_2 < r_2, ... then clearly l_1 + l_2 + ... < r_1 + r_2 + ... *)
 
-let _ = Went_negative
+(* First, we pick the set of 2k elements from which to draw our two k-sets. *)
+let count_subset_pair_unions ~n ~k = Number_theory.Int.binomial n (2 * k)
 
-let number_of_arrangements_of_2k_elements_to_test k =
+exception Not_dominating
+
+(* Count the number of permutations of k copies each of (L, R) where L is
+   "dominated" by R. *)
+let count_non_dominating_set_pairs ~k =
   let seq = List.init k ~f:(fun _ -> `Left) @ List.init k ~f:(fun _ -> `Right) in
   Sequences.permutations
     seq
@@ -16,39 +25,36 @@ let number_of_arrangements_of_2k_elements_to_test k =
          | `Left -> 0
          | `Right -> 1))
   |> Sequence.take_while ~f:(fun perm -> Poly.( = ) (List.hd_exn perm) `Left)
-  |> Sequence.filter ~f:(fun perm ->
-    Poly.( = )
-      `Need_to_test
-      (try
-         if debug
-         then Debug.eprint_s [%message "testing" (perm : [`Left | `Right] list)];
-         let rec loop perm left_minus_right_in_prefix =
-           match perm with
-           | [] -> `Obvious
-           | `Left :: tl -> loop tl (left_minus_right_in_prefix + 1)
-           | `Right :: tl ->
-             let left_minus_right_in_prefix = left_minus_right_in_prefix - 1 in
-             if left_minus_right_in_prefix < 0 then raise Went_negative;
-             loop tl left_minus_right_in_prefix
-         in
-         loop perm 0
-       with
-       | Went_negative -> `Need_to_test))
-  |> Sequence.to_list
-  |> fun x ->
-  if debug then Debug.eprint_s [%message "permutations" (x : [`Left | `Right] list list)];
-  x |> List.length
+  |> Sequence.count ~f:(fun perm ->
+    let non_dominating =
+      try
+        let rec loop perm left_minus_right_in_prefix =
+          match perm with
+          | [] -> false
+          | `Left :: tl -> loop tl (left_minus_right_in_prefix + 1)
+          | `Right :: tl ->
+            let left_minus_right_in_prefix = left_minus_right_in_prefix - 1 in
+            if left_minus_right_in_prefix < 0 then raise Not_dominating;
+            loop tl left_minus_right_in_prefix
+        in
+        loop perm 0
+      with
+      | Not_dominating -> true
+    in
+    if debug && non_dominating
+    then Debug.eprint_s [%message "dominating" (perm : [`Left | `Right] list)];
+    non_dominating)
 ;;
 
-(* We only need to test disjoint subsets of the same size, since rule 2 takes
-   care of subsets of different sizes. *)
-let number_of_subset_pairs_to_be_tested n =
+(* Count the total number of pairs of disjoint subsets from a set of [n]
+   elements that need to be tested for rule 1. *)
+let count_subset_pairs_to_be_tested ~n =
   List.range 1 (n / 2) ~stop:`inclusive
   |> List.sum
        (module Int)
        ~f:(fun k ->
-         let a = number_of_subset_pair_unions n k in
-         let b = number_of_arrangements_of_2k_elements_to_test k in
+         let a = count_subset_pair_unions ~n ~k in
+         let b = count_non_dominating_set_pairs ~k in
          if debug
          then Debug.eprint_s [%message "" (k : int) (a : int) (b : int) (a * b : int)];
          a * b)
@@ -56,7 +62,7 @@ let number_of_subset_pairs_to_be_tested n =
 
 module M = struct
   let problem = Number 106
-  let main () = number_of_subset_pairs_to_be_tested 12 |> printf "%d\n"
+  let main () = count_subset_pairs_to_be_tested ~n:12 |> printf "%d\n"
 
   (* 21384
      0.456ms *)

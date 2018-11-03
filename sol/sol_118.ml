f9f996b36c93c2e8ef35079e1036d0540b2e9443
Aaron L. Zeng <me@bcc32.com>
2018-11-02 22:03:21 -0400

    Solve 118

---
+++ sol/sol_118.ml
@@ -0,0 +1,55 @@
+open! Core
+open! Import
+
+let set_diff a b ~equal =
+  match a, b with
+  | [], _
+  | _, [] -> a
+  | _ -> List.filter a ~f:(Fn.non (List.mem b ~equal))
+;;
+
+let iter_subsets set ~f =
+  let rec loop set subset =
+    match set with
+    | [] -> f subset
+    | hd :: tl ->
+      loop tl subset;
+      loop tl (hd :: subset)
+  in
+  loop set []
+;;
+
+let iter_allprime_subsets set ~f =
+  let rec loop set min subsets =
+    match set with
+    | [] -> f subsets
+    | set ->
+      iter_subsets set ~f:(fun subset ->
+        Sequences.iter_permutations subset ~compare:Int.compare ~f:(fun n ->
+          let n =
+            n
+            |> Array.Permissioned.to_sequence_mutable
+            |> Number_theory.Int.int_of_digits
+          in
+          if n >= min && Number_theory.Int.is_prime n
+          then loop (set_diff set subset ~equal:Int.equal) (n + 1) (subset :: subsets)))
+  in
+  loop set 0 []
+;;
+
+module M = struct
+  let problem = Number 118
+
+  let main () =
+    let count = ref 0 in
+    iter_allprime_subsets [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] ~f:(fun subsets ->
+      if debug then Debug.eprint_s [%sexp (subsets : int list list)];
+      incr count);
+    printf "%d\n" !count
+  ;;
+
+  (* 44680
+     4.0339s *)
+end
+
+include Solution.Make (M)

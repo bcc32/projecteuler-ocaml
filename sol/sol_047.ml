95965a7f38bac0d26b4c28a3a58dd3ab78a42822
Aaron L. Zeng <me@bcc32.com>
2018-11-13 11:41:30 -0500

    47: avoid memoizing the prime factor sequence
    - Directly construct the sequence of consecutive element groups.
    - Extend the Boyer-Moore analogy.  Doesn't really do much for performance.
    
    About 30% speedup.

--- sol/sol_047.ml
+++ sol/sol_047.ml
@@ -12,28 +12,36 @@ module M = struct
         n, Number_theory.Int.prime_factor n |> List.length)
     in
     let each_consecutive_4 =
-      let a = distinct_prime_factors |> Sequence.memoize in
-      let b = a |> Sequence.tl_eagerly_exn in
-      let c = b |> Sequence.tl_eagerly_exn in
-      let d = c |> Sequence.tl_eagerly_exn in
-      (* TODO Replace this with a zip-based applicative. *)
-      Sequence.zip (Sequence.zip a b) (Sequence.zip c d)
-      |> Sequence.map ~f:(fun ((a, b), (c, d)) -> a, b, c, d)
+      let dpf = distinct_prime_factors in
+      let a, dpf = uw (Sequence.next dpf) in
+      let b, dpf = uw (Sequence.next dpf) in
+      let c, dpf = uw (Sequence.next dpf) in
+      Sequence.folding_map dpf ~init:(a, b, c) ~f:(fun (a, b, c) d ->
+        (b, c, d), (a, b, c, d))
     in
-    each_consecutive_4
-    |> Sequence.find_map ~f:(function (a, apf), (_, bpf), (_, cpf), (_, dpf) ->
+    let rec loop seq =
+      let ((a, apf), (_, bpf), (_, cpf), (_, dpf)), seq = uw (Sequence.next seq) in
       (* Check [dpf] first à la Boyer-Moore:
-         https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_string-search_algorithm
+         https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_string-search_algorithm.
 
-         Ideally, we'd like to skip multiple 4-tuples immediately if [dpf <>
-         4], but that's a bit harder to implement. *)
-      if dpf = 4 && cpf = 4 && bpf = 4 && apf = 4 then Some a else None)
-    |> uw
-    |> printf "%d\n"
+         This doesn't actually buy us much performance-wise, but it's cool, so
+         whatever. *)
+      let step n = loop (Sequence.drop_eagerly seq n) in
+      if dpf <> 4
+      then step 3
+      else if cpf <> 4
+      then step 2
+      else if bpf <> 4
+      then step 1
+      else if apf <> 4
+      then step 0
+      else a
+    in
+    loop each_consecutive_4 |> printf "%d\n"
   ;;
 
   (* 134043
-     192.31ms *)
+     98.823ms *)
 end
 
 include Solution.Make (M)

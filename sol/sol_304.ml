e08fa78a710dcc2e9729a6a432c24bc5549c5aaf
Aaron L. Zeng <me@bcc32.com>
2018-11-17 17:44:46 -0500

    Work on 304

---
+++ sol/sol_304.ml
@@ -0,0 +1,31 @@
+open! Core
+open! Import
+
+let a =
+  lazy
+    (let a1 = Number_theory.Int.next_prime 100_000_000_000_000 in
+     Sequence.unfold ~init:a1 ~f:(fun a ->
+       if debug then Debug.eprintf "%d" a;
+       let a' = Number_theory.Int.next_prime a in
+       Some (a', a')))
+  |> Sequence.of_lazy
+;;
+
+let b = Sequence.map a ~f:Number_theory.Int.fast_fibonacci
+
+module Int_modulo = struct
+  type t = int [@@deriving sexp]
+
+  let modulus = 1234567891011
+  let zero = 0
+  let ( + ) a b = (a + b) % modulus
+  let ( - ) a b = (a - b) % modulus
+end
+
+let main () =
+  Sequence.take b 100_000 |> Sequence.sum (module Int_modulo) ~f:Fn.id |> printf "%d\n"
+;;
+
+(* incomplete *)
+
+include (val Solution.make ~problem:(Number 304) ~main)

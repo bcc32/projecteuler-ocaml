d1afb7de2ef4fd98d2b5708a7981227ceb931398
Aaron L. Zeng <me@bcc32.com>
2018-11-26 18:02:55 -0500

    Simplify parsing loop to avoid ref cell

--- data/parse.ml
+++ data/parse.ml
@@ -18,14 +18,10 @@ let comma_separated_quoted_words string =
       (Re.seq [ Re.char '"'; Re.group (Re.rep (Re.compl [ Re.char '"' ])); Re.char '"' ])
   in
   let next_match = Re.all_gen word string in
-  let result = ref [] in
-  let rec loop () =
+  let rec loop words =
     match next_match () with
-    | None -> ()
-    | Some groups ->
-      result := Re.Group.get groups 1 :: !result;
-      loop ()
+    | None -> List.rev words
+    | Some groups -> loop (Re.Group.get groups 1 :: words)
   in
-  loop ();
-  List.rev !result
+  loop []
 ;;

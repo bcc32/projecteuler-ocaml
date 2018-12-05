65f4d5eef01dc79c9e4e3b3a01ffc04ac4340204
Aaron L. Zeng <me@bcc32.com>
2018-12-05 11:36:29 -0500

    34: manually keep track of digits
    Since digits are easy to incrementally update for a range of consecutive
    integers, it's relatively cheap and avoids integer division otherwise necessary
    to calculate digits.
    
    About 94% speedup.

--- sol/sol_034.ml
+++ sol/sol_034.ml
@@ -7,13 +7,48 @@ module M = struct
   (* Derived by observing that 9! * n < 10^(n - 1) for all n > 7. *)
   let max_digits = 7
 
+  let incr digits =
+    let rec loop i =
+      if i >= 0
+      then
+        if digits.(i) = 9
+        then (
+          digits.(i) <- 0;
+          loop (i - 1))
+        else digits.(i) <- digits.(i) + 1
+      else failwith "no digits to increment"
+    in
+    loop (Array.length digits - 1)
+  ;;
+
   let main () =
     let factorial = Array.init 10 ~f:Number_theory.Int.factorial in
-    Sequence.range 10 (Int.pow 10 max_digits)
-    |> Sequence.filter ~f:(fun n ->
-      n = Number_theory.Int.fold_digits n ~init:0 ~f:(fun ac d -> ac + factorial.(d)))
-    |> Sequence.sum (module Int) ~f:Fn.id
-    |> printf "%d\n"
+    let digits = Array.create ~len:max_digits 0 in
+    (* start at 10 *)
+    digits.(Array.length digits - 1) <- 9;
+    let sum = ref 0 in
+    for num_digits = 2 to max_digits do
+      for n = Int.pow 10 (num_digits - 1) to Int.pow 10 num_digits - 1 do
+        incr digits;
+        if debug
+        then (
+          let n' = Array.fold digits ~init:0 ~f:(fun acc x -> (acc * 10) + x) in
+          [%test_eq: int] n n');
+        let n' =
+          (* actually substantially faster than Array.sum *)
+          let sum = ref 0 in
+          for i = Array.length digits - num_digits to Array.length digits - 1 do
+            sum := !sum + factorial.(digits.(i))
+          done;
+          !sum
+        in
+        if n = n'
+        then (
+          if debug then Debug.eprintf "%d" n;
+          sum := !sum + n)
+      done
+    done;
+    printf "%d\n" !sum
   ;;
 end
 

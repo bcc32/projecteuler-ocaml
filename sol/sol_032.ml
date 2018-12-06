8ec66d346ea4956abf9f31e0f31a9ff351a964f0
Aaron L. Zeng <me@bcc32.com>
2018-12-06 12:45:59 -0500

    32: short-circuit on duplicate digits
    Keep the list sorted and merge into it (might be a bit slower than just doing
    the O(n^2) thing each time and sorting at the end).
    
    About 2x faster.

--- sol/sol_032.ml
+++ sol/sol_032.ml
@@ -4,16 +4,31 @@ open! Import
 (* WLOG a < b, so a has at most 2 digits *)
 let pandigital () =
   let expected_digits = List.range 1 10 in
+  let merge n ~into:sorted_digits =
+    List.merge
+      ~compare:Int.compare
+      (List.sort ~compare:Int.compare (Number_theory.Int.digits_of_int n))
+      sorted_digits
+  in
+  let has_consecutive_duplicate =
+    Option.is_some << List.find_consecutive_duplicate ~equal:Int.equal
+  in
   let open Sequence.Let_syntax in
   let%bind a = Sequence.range 1 100 in
-  let digits = Number_theory.Int.digits_of_int a in
-  let%bind b = Sequence.range (a + 1) (Number_theory.Int.isqrt (1_000_000_000 / a)) in
-  let digits = digits @ Number_theory.Int.digits_of_int b in
-  let c = a * b in
-  let digits = digits @ Number_theory.Int.digits_of_int c in
-  if [%compare.equal: int list] expected_digits (List.sort digits ~compare:Int.compare)
-  then return c
-  else Sequence.empty
+  let digits = merge a ~into:[] in
+  if has_consecutive_duplicate digits
+  then Sequence.empty
+  else (
+    let%bind b = Sequence.range (a + 1) (Number_theory.Int.isqrt (1_000_000_000 / a)) in
+    let digits = merge b ~into:digits in
+    if has_consecutive_duplicate digits
+    then Sequence.empty
+    else (
+      let c = a * b in
+      let digits = merge c ~into:digits in
+      if [%compare.equal: int list] expected_digits digits
+      then return c
+      else Sequence.empty))
 ;;
 
 module M = struct
@@ -27,7 +42,7 @@ module M = struct
     |> printf "%d\n"
   ;;
 
-  (* 404.011ms *)
+  (* 200ms *)
   let%expect_test "answer" =
     main ();
     [%expect {| 45228 |}]

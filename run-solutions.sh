ae20cfb9a644821da6bf4836d979aa914edbfaf6
Aaron L. Zeng <me@bcc32.com>
2019-03-10 15:24:19 -0400

    Shellcheck run-solutions.sh

--- run-solutions.sh
+++ run-solutions.sh
@@ -2,18 +2,26 @@
 
 set -euxo pipefail
 
-function list_solutions {
+list_solutions() {
     dune exec -p euler -- euler 2>&1 | awk '/Problem/ { print $1 }' | grep -v naive
 }
+
+run_solution() {
+    sol=$1
+    echo "$sol"
+    dune exec -p euler --no-build -- euler "$sol" -time
+}
+export -f run_solution
 
 echo "building..."
 dune build -p euler
 echo "done building"
 
-if which parallel >/dev/null 2>&1; then
-    parallel -k -- echo {1} '&&' dune exec --no-build -p euler -- euler {1} -time ::: $(list_solutions)
+if command -v parallel >/dev/null 2>&1; then
+    list_solutions \
+        | parallel --keep-order run_solution
 else
     for i in $(list_solutions); do
-        dune exec --no-build -p euler -- euler $i -time
+        run_solution "$i"
     done
 fi

4bfae4404e9f8e88cf22021abf60f811ef93b214
Aaron L. Zeng <me@bcc32.com>
2019-04-04 17:26:40 -0400

    14: Use a plain int array with sentinel value
    22% speedup on a different box.  Timing is speculative.

--- sol/sol_014.ml
+++ sol/sol_014.ml
@@ -7,14 +7,14 @@ module M = struct
   let collatz n = if n mod 2 = 0 then n / 2 else (3 * n) + 1
 
   let rec collatz_length =
-    let cache = Option_array.create ~len:(limit + 1) in
-    Option_array.set_some cache 1 1;
+    let cache = Array.create (-1) ~len:(limit + 1) in
+    cache.(1) <- 1;
     fun n ->
-      if n <= limit && Option_array.is_some cache n
-      then Option_array.unsafe_get_some_exn cache n
+      if n <= limit && cache.(n) <> -1
+      then cache.(n)
       else (
         let length = collatz_length (collatz n) + 1 in
-        if n <= limit then Option_array.set_some cache n length;
+        if n <= limit then cache.(n) <- length;
         length)
   ;;
 
@@ -26,7 +26,7 @@ module M = struct
     |> printf "%d\n"
   ;;
 
-  (* 94ms *)
+  (* 72ms *)
   let%expect_test "answer" =
     main ();
     [%expect {| 837799 |}]

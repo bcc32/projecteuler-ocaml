87206fccdd537422cef308ee49a9ecf7c05a6005
Aaron L. Zeng <me@bcc32.com>
2019-04-11 14:24:47 -0400

    Parse: leverage `Seq` from stdlib

--- data/parse.ml
+++ data/parse.ml
@@ -17,11 +17,7 @@ let comma_separated_quoted_words string =
     Re.compile
       (Re.seq [ Re.char '"'; Re.group (Re.rep (Re.compl [ Re.char '"' ])); Re.char '"' ])
   in
-  let next_match = Re.all_gen word string in
-  let rec loop words =
-    match next_match () with
-    | None -> List.rev words
-    | Some groups -> loop (Re.Group.get groups 1 :: words)
-  in
-  loop []
+  Re.all_seq word string
+  |> Seq.map (fun groups -> Re.Group.get groups 1)
+  |> Caml.List.of_seq
 ;;

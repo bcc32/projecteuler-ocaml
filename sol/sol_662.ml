68793859bf47582b6d52954ab341c884d50e07d5
Aaron L. Zeng <me@bcc32.com>
2019-05-01 18:24:19 -0400

    662: Use forward DP instead of memoization
    - Don't modulus on every addition.  Since each sum contains about 80
      or so terms, we can just mod at the end of each sum.
    - Sorting by (x + y) instead of (x, y) is about 3% or so faster.
    
    About 2x speedup.

--- sol/sol_662.ml
+++ sol/sol_662.ml
@@ -2,6 +2,7 @@ open! Core
 open! Import
 
 let grid_length = 10_000
+let modulus = 1_000_000_007
 
 let fibonacci =
   Number_theory.Int.fibonacci
@@ -37,7 +38,7 @@ let moves =
     assert (x <> y);
     [ x, y; y, x ])
   |> List.filter ~f:(fun (x, y) -> x <= grid_length && y <= grid_length)
-  |> List.sort ~compare:[%compare: int * int]
+  |> List.sort ~compare:(Comparable.lift Int.compare ~f:(fun (x, y) -> x + y))
 ;;
 
 let%expect_test "moves" =
@@ -45,129 +46,113 @@ let%expect_test "moves" =
   [%expect
     {|
     ((0    1)
-     (0    2)
-     (0    3)
-     (0    5)
-     (0    8)
-     (0    13)
-     (0    21)
-     (0    34)
-     (0    55)
-     (0    89)
-     (0    144)
-     (0    233)
-     (0    377)
-     (0    610)
-     (0    987)
-     (0    1597)
-     (0    2584)
-     (0    4181)
-     (0    6765)
      (1    0)
+     (0    2)
      (2    0)
+     (0    3)
      (3    0)
+     (0    5)
+     (5    0)
      (3    4)
      (4    3)
-     (5    0)
-     (5    12)
+     (0    8)
      (8    0)
-     (12   5)
+     (0    13)
      (13   0)
-     (16   30)
+     (5    12)
+     (12   5)
+     (0    21)
      (21   0)
-     (30   16)
-     (33   44)
+     (0    34)
      (34   0)
-     (39   80)
-     (44   33)
+     (30   16)
+     (16   30)
+     (0    55)
      (55   0)
-     (80   39)
+     (33   44)
+     (44   33)
+     (0    89)
      (89   0)
-     (105  208)
-     (110  600)
-     (135  352)
+     (39   80)
+     (80   39)
+     (0    144)
      (144  0)
-     (145  348)
-     (152  345)
-     (208  105)
+     (0    233)
      (233  0)
-     (260  273)
-     (272  546)
-     (273  260)
-     (345  152)
-     (348  145)
-     (352  135)
-     (366  488)
+     (105  208)
+     (208  105)
+     (0    377)
      (377  0)
+     (135  352)
+     (352  135)
+     (145  348)
+     (348  145)
+     (345  152)
+     (152  345)
+     (273  260)
+     (260  273)
+     (0    610)
+     (610  0)
+     (110  600)
+     (600  110)
+     (546  272)
+     (272  546)
+     (366  488)
+     (488  366)
      (414  448)
      (448  414)
-     (488  366)
-     (546  272)
-     (555  4144)
-     (600  110)
-     (610  0)
-     (715  1428)
-     (819  4100)
+     (0    987)
      (987  0)
-     (1216 2280)
-     (1356 3955)
-     (1428 715)
-     (1485 6600)
+     (0    1597)
      (1597 0)
-     (1869 3740)
-     (2280 1216)
+     (715  1428)
+     (1428 715)
+     (0    2584)
      (2584 0)
-     (2772 6171)
-     (3740 1869)
-     (3955 1356)
-     (4059 5412)
-     (4100 819)
-     (4144 555)
+     (2280 1216)
+     (1216 2280)
+     (0    4181)
      (4181 0)
-     (4389 5148)
-     (4896 9790)
-     (5148 4389)
-     (5412 4059)
-     (6171 2772)
-     (6600 1485)
+     (555  4144)
+     (4144 555)
+     (819  4100)
+     (4100 819)
+     (3955 1356)
+     (1356 3955)
+     (1869 3740)
+     (3740 1869)
+     (0    6765)
      (6765 0)
-     (9790 4896)) |}]
+     (1485 6600)
+     (6600 1485)
+     (6171 2772)
+     (2772 6171)
+     (4059 5412)
+     (5412 4059)
+     (4389 5148)
+     (5148 4389)
+     (9790 4896)
+     (4896 9790)) |}]
 ;;
 
-module Int_with_modulus = struct
-  type t = int
-
-  let zero = 0
-  let modulus = 1_000_000_007
-  let ( + ) x y = (x + y) mod modulus
-end
-
-let rec ways =
-  let cache = Array.create (-1) ~len:((grid_length + 1) * (grid_length + 1)) in
-  let findi_or_add w h ~default =
-    let index = (grid_length * w) + h in
-    let cached = cache.(index) in
-    if cached = -1
-    then (
-      let x = default w h in
-      cache.(index) <- x;
-      x)
-    else cached
-  in
-  fun w h ->
-    if w < 0 || h < 0
-    then 0
-    else if w = 0 && h = 0
-    then 1
-    else if w > h
-    then ways h w
-    else
-      findi_or_add w h ~default:(fun w h ->
-        if debug then Debug.eprint_s [%message (w : int) (h : int)];
-        List.sum
-          (module Int_with_modulus)
-          moves
-          ~f:(fun (dx, dy) -> ways (w - dx) (h - dy)))
+let ways w h =
+  let cache = Array.create 0 ~len:((w + 1) * (h + 1)) in
+  let index x y = ((h + 1) * x) + y in
+  let get x y = cache.(index x y) in
+  cache.(0) <- 1;
+  for x = 0 to w do
+    for y = 0 to h do
+      if x > 0 || y > 0
+      then
+        cache.(index x y)
+        <- List.sum
+             (module Int)
+             moves
+             ~f:(fun (dx, dy) -> if x >= dx && y >= dy then get (x - dx) (y - dy) else 0)
+           mod modulus
+    done
+  done;
+  get w h
 ;;
 
 let%expect_test "F(3, 4)" =
@@ -189,7 +174,7 @@ module M = struct
   ;;
 
   (* 860873428
-     3m26.693256358s *)
+     1m42.126623087s *)
 end
 
 include Solution.Make (M)

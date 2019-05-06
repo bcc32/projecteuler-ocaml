8e0a53e9f76004681423b411a8b079ce4cd44c0e
Aaron L. Zeng <me@bcc32.com>
2019-05-06 18:42:13 -0400

    Shellcheck data/gen-problem.sh

--- data/gen-problem.sh
+++ data/gen-problem.sh
@@ -4,7 +4,7 @@ set -eu
 
 num=$1
 
-if grep -F '|}' $num.txt >/dev/null 2>&1; then
+if grep -F '|}' "$num.txt" >/dev/null 2>&1; then
     >&2 echo "$num.txt contains delimited quote, aborting"
     exit 1
 fi

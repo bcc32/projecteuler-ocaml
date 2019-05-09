530e7b2f1f1b51f238e940daa6f838b368c31d2d
Aaron L. Zeng <me@bcc32.com>
2019-05-09 15:27:51 -0400

    Add some proof

--- sol/sol_659.ml
+++ sol/sol_659.ml
@@ -1,6 +1,25 @@
 open! Core
 open! Import
 
+(* If [p = 2], then no two consecutive terms of the sequence [n^2+m] can be divisible by
+   [p], since [n^2] alternates between odd and even.
+
+   Given an odd prime [p = 2a+1], [p] divides two consecutive terms of the sequence
+   [n^2+m] iff [p] divides [a^2 + m].
+
+   Proof:
+
+   First, observe that [b^2+m = (-b)^2+m = (p-b)^2+m], mod [p].
+
+   Consider two consecutive terms [b^2+m, (b+1)^2+m], where [b < a].  Their difference is
+   [2b+1 < p], so it is impossible for [b^2+m = (b+1)^2+m] mod [p].  We also know that
+   [a^2+m = (-a)^2+m = (a+1)^2+m], mod [p].  Therefore, the only pair of consecutive equal
+   (mod p) terms can be [a^2 + m] and [(a+1)^2 + m].
+
+   If [p] divides [a^2 + m], then it also divides [(a+1)^2 + m], so we only have to check
+   [p | a^2 + m] to see if there are two consecutive terms in the sequence divisible by
+   [p]. *)
+
 let primes =
   Number_theory.Int.primes
   |> Fn.flip Sequence.drop_eagerly 1

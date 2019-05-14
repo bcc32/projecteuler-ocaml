bb932d7cd260abeaa2e94d6b0684d767628f6690
Aaron L. Zeng <me@bcc32.com>
2019-05-14 18:17:46 -0400

    Replace confusing partially-applied comparison

--- sol/sol_019.ml
+++ sol/sol_019.ml
@@ -8,7 +8,7 @@ module M = struct
     let start = Date.of_string "1901-01-01" in
     let stop = Date.of_string "2000-12-31" in
     Sequence.unfold ~init:start ~f:(fun d -> Some (d, Date.add_months d 1))
-    |> Sequence.take_while ~f:(Date.( >= ) stop)
+    |> Sequence.take_while ~f:(fun x -> Date.O.(x <= stop))
     |> Sequence.count ~f:(fun d -> [%equal: Day_of_week.t] Sun (Date.day_of_week d))
     |> printf "%d\n"
   ;;

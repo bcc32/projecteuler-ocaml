2c0ac57e42df90bf36cf1a916dee28c5db330e62
Aaron L. Zeng <me@bcc32.com>
2019-06-04 12:56:48 -0400

    Use quickcheck in integration test

--- test/test_integrate.ml
+++ test/test_integrate.ml
@@ -3,22 +3,25 @@ open! Import
 
 let%test_unit "integrate" =
   let open Bignum.O in
-  let f x = x ** 2 in
-  let y =
-    Numerics.Bignum.integrate
-      ()
-      ~method_:`Simpson's_rule
-      ~f
-      ~lo:(of_int 1)
-      ~hi:(of_int 5)
-      ~intervals:2
-  in
-  let expect =
-    let interval ~f ~x_lo ~x_mi ~x_hi =
-      let f = f << of_int in
-      Int.O.(x_hi - x_lo) // 6 * (f x_lo + (of_int 4 * f x_mi) + f x_hi)
-    in
-    interval ~f ~x_lo:1 ~x_mi:2 ~x_hi:3 + interval ~f ~x_lo:3 ~x_mi:4 ~x_hi:5
-  in
-  [%test_result: Bignum.t] y ~expect
+  Quickcheck.test
+    [%quickcheck.generator: Bignum.t -> Bignum.t]
+    ~examples:[ (fun x -> x ** 2) ]
+    ~f:(fun f ->
+      let y =
+        Numerics.Bignum.integrate
+          ()
+          ~method_:`Simpson's_rule
+          ~f
+          ~lo:(of_int 1)
+          ~hi:(of_int 5)
+          ~intervals:2
+      in
+      let expect =
+        let interval ~f ~x_lo ~x_mi ~x_hi =
+          let f = f << of_int in
+          Int.O.(x_hi - x_lo) // 6 * (f x_lo + (of_int 4 * f x_mi) + f x_hi)
+        in
+        interval ~f ~x_lo:1 ~x_mi:2 ~x_hi:3 + interval ~f ~x_lo:3 ~x_mi:4 ~x_hi:5
+      in
+      [%test_result: Bignum.t] y ~expect)
 ;;

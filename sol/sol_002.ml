4692e7347ced9bdec2158887879f912560d12858
Aaron L. Zeng <me@bcc32.com>
2019-06-14 11:10:57 -0400

    2: Use library Fibonacci sequence
    Marginally slower, but it's fine.

--- sol/sol_002.ml
+++ sol/sol_002.ml
@@ -1,15 +1,11 @@
 open! Core
 open! Import
 
-let fibs n =
-  let rec iter a b acc = if b > n then acc else iter b (a + b) (b :: acc) in
-  iter 0 1 [] |> List.rev
-;;
-
 let main () =
-  fibs 4000000
-  |> List.filter ~f:(fun x -> x mod 2 = 0)
-  |> List.sum (module Int) ~f:Fn.id
+  Number_theory.Int.fibonacci
+  |> Sequence.take_while ~f:(fun x -> x <= 4000000)
+  |> Sequence.filter ~f:(fun x -> x mod 2 = 0)
+  |> Sequence.sum (module Int) ~f:Fn.id
   |> printf "%d\n"
 ;;
 

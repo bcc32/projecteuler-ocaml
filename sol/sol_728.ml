7ecac69eea43cc572a8c508235eee8e4117990f0
Aaron L. Zeng <me@bcc32.com>
2020-10-04 15:52:45 -0400

    WIP 728

--- sol/sol_728.ml
+++ sol/sol_728.ml
@@ -31,6 +31,45 @@ open! Import
    is equal to 2^n divided by the number of move sequences equivalent to the
    null move sequence, including the null move sequence. *)
 
+module For_63_bits = struct
+  let get_bit_norm ~n_bits int ~bit =
+    let bit = if bit < 0 then bit + n_bits else bit in
+    (int lsr bit) land 1
+  ;;
+
+  let move_sequences_equivalent_to_null n k =
+    Sequence.range 0 (Int.pow 2 n)
+    |> Sequence.count ~f:(fun move_bits ->
+      let state =
+        let state_bits =
+          List.init n ~f:(fun state_bit ->
+            List.init k ~f:(fun move_bit ->
+              get_bit_norm ~n_bits:n move_bits ~bit:(state_bit - move_bit))
+            |> List.fold ~init:0 ~f:( lxor ))
+        in
+        List.fold state_bits ~init:0 ~f:(fun acc bit -> (acc lsl 1) lor bit)
+      in
+      state = 0)
+  ;;
+
+  let distinct_states n k = Int.pow 2 n / move_sequences_equivalent_to_null n k
+
+  let%expect_test "F(3,2)" =
+    distinct_states 3 2 |> [%sexp_of: int] |> print_s;
+    [%expect {| 4 |}]
+  ;;
+
+  let%expect_test "F(8,3)" =
+    distinct_states 8 3 |> [%sexp_of: int] |> print_s;
+    [%expect {| 256 |}]
+  ;;
+
+  let%expect_test "F(9,3)" =
+    distinct_states 9 3 |> [%sexp_of: int] |> print_s;
+    [%expect {| 128 |}]
+  ;;
+end
+
 let main () = raise_s [%message "unimplemented" [%here]]
 
-include (val Solution.make ~problem:(Number ___) ~main)
+include (val Solution.make ~problem:(Number 728) ~main)

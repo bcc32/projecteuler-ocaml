d87a3c37417c7452e1ac59abcd1ccbceda5b0c36
Aaron L. Zeng <me@bcc32.com>
2020-10-04 22:07:31 -0400

    Working

--- sol/sol_728.ml
+++ sol/sol_728.ml
@@ -70,16 +70,22 @@ module For_63_bits = struct
   ;;
 
   let%expect_test "table" =
-    for n = 1 to 14 do
+    let max = 15 in
+    printf "%2d\t" 0;
+    for k = 1 to max do
+      printf "%d\t" k
+    done;
+    printf "\n";
+    for n = 1 to max do
       printf "%2d\t" n;
       for k = 1 to n do
         printf "%d\t" (distinct_states n k)
-        (* printf "n = %d, k = %d, F(n,k) = %d\n" n k (distinct_states n k) *)
       done;
       printf "\n"
     done;
     [%expect
       {|
+       0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15
        1	2
        2	4	2
        3	8	4	2
@@ -93,7 +99,113 @@ module For_63_bits = struct
       11	2048	1024	2048	1024	2048	1024	2048	1024	2048	1024	2
       12	4096	2048	1024	512	4096	128	4096	256	1024	2048	4096	2
       13	8192	4096	8192	4096	8192	4096	8192	4096	8192	4096	8192	4096	2
-      14	16384	8192	16384	4096	16384	8192	256	4096	16384	8192	16384	4096	16384	2 |}]
+      14	16384	8192	16384	4096	16384	8192	256	4096	16384	8192	16384	4096	16384	2
+      15	32768	16384	8192	16384	2048	4096	32768	16384	8192	1024	32768	4096	32768	16384	2 |}]
+  ;;
+
+  (* For each row n, the entries mostly alternate between two different powers
+     of 2, 2^n and 2^(n - 1), except:
+
+     For n = k, F(n, k) = 2.
+
+     For gcd(n, k) > 1, the entries may be a smaller power of 2.
+
+     In particular,
+
+     F(n, k) = 2 if n = k
+
+     F(n, k) = 2^(n - a) where
+
+     a = k mod 2 + b
+
+     b = 2p if n and k share a prime factor greater than 2 (p is the index of
+     that prime number, where p = 1 if the prime factor is 3)
+
+     F(n, k) = 2^n otherwise *)
+
+  let%expect_test "pattern" =
+    let max = 15 in
+    for n = 1 to max do
+      for k = 1 to n do
+        let base = Int.pow 2 n in
+        let diff = Int.floor_log2 (base / distinct_states n k) in
+        if diff > 0 then printf "%d %d %d\n" n k diff
+      done
+    done;
+    [%expect
+      {|
+      2 2 1
+      3 2 1
+      3 3 2
+      4 2 1
+      4 4 3
+      5 2 1
+      5 4 1
+      5 5 4
+      6 2 1
+      6 3 2
+      6 4 2
+      6 6 5
+      7 2 1
+      7 4 1
+      7 6 1
+      7 7 6
+      8 2 1
+      8 4 3
+      8 6 1
+      8 8 7
+      9 2 1
+      9 3 2
+      9 4 1
+      9 6 3
+      9 8 1
+      9 9 8
+      10 2 1
+      10 4 2
+      10 5 4
+      10 6 1
+      10 8 2
+      10 10 9
+      11 2 1
+      11 4 1
+      11 6 1
+      11 8 1
+      11 10 1
+      11 11 10
+      12 2 1
+      12 3 2
+      12 4 3
+      12 6 5
+      12 8 4
+      12 9 2
+      12 10 1
+      12 12 11
+      13 2 1
+      13 4 1
+      13 6 1
+      13 8 1
+      13 10 1
+      13 12 1
+      13 13 12
+      14 2 1
+      14 4 2
+      14 6 1
+      14 7 6
+      14 8 2
+      14 10 1
+      14 12 2
+      14 14 13
+      15 2 1
+      15 3 2
+      15 4 1
+      15 5 4
+      15 6 3
+      15 8 1
+      15 9 2
+      15 10 5
+      15 12 3
+      15 14 1
+      15 15 14 |}]
   ;;
 
   let s n =

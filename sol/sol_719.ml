b0353368a52db1bff3c63ab104f0210d8e915303
Aaron L. Zeng <me@bcc32.com>
2020-10-05 21:19:14 -0400

    719: Directly check for attainability
    This allows us to short-circuit much more and is something like 25x faster.

--- sol/sol_719.ml
+++ sol/sol_719.ml
@@ -1,49 +1,21 @@
 open! Core
 open! Import
 
-let sum_with_sep_mask ~n_digits ~sep_mask =
-  (* place is tens power of next digit to be added to term *)
-  let rec loop i ~term ~place ~sum ~mask =
-    if debug
-    then
-      Debug.eprint_s
-        [%message [%here] (i : int) (term : int) (place : int) (sum : int) (mask : int)];
-    if i < 0
-    then term + sum
-    else if mask land 1 <> 0
-    then
-      (* separate *)
-      loop (i - 1) ~term:n_digits.(i) ~place:10 ~sum:(term + sum) ~mask:(mask lsr 1)
-    else
-      (* same term *)
-      loop
-        (i - 1)
-        ~term:((place * n_digits.(i)) + term)
-        ~place:(10 * place)
-        ~sum
-        ~mask:(mask lsr 1)
-  in
-  loop
-    (Array.length n_digits - 2)
-    ~term:(Array.last n_digits)
-    ~place:10
-    ~sum:0
-    ~mask:sep_mask
-;;
-
-let%expect_test "sum_with_sep_mask" =
-  let n_digits = [| 1; 2; 4; 8 |] in
-  let test (sep_mask, expect) =
-    let sum = sum_with_sep_mask ~n_digits ~sep_mask in
-    require_equal
-      [%here]
-      (module Int)
-      sum
-      expect
-      ~if_false_then_print_s:(lazy [%message (sep_mask : Int.Hex.t)])
-  in
-  List.iter ~f:test [ 0b000, 1248; 0b111, 1 + 2 + 4 + 8; 0b010, 12 + 48 ];
-  [%expect {| |}]
+(* Inspired by NabiNaga's solution at
+   https://projecteuler.net/thread=719;page=3#365650 *)
+let rec can_sum ~digits_of:n ~target =
+  if n = target
+  then true
+  else if n < target
+  then false
+  else (
+    let m = ref 10 in
+    with_return (fun { return } ->
+      while !m < n do
+        if can_sum ~digits_of:(n / !m) ~target:(target - (n % !m)) then return true;
+        m := !m * 10
+      done;
+      false))
 ;;
 
 (* As noted by ecnerwala, https://projecteuler.net/thread=719#359366,
@@ -56,12 +28,9 @@ let is_s_number ~sqrt ~n =
   let mod_9 = sqrt % 9 in
   if not (mod_9 = 0 || mod_9 = 1)
   then None
-  else (
-    let n_digits = Number_theory.Int.As_base10.to_array n in
-    Sequence.range 0 (Int.pow 2 (Array.length n_digits - 1))
-    |> Sequence.find_map ~f:(fun sep_mask ->
-      let summed = sum_with_sep_mask ~n_digits ~sep_mask in
-      if sqrt = summed then Some (sqrt, n, sep_mask) else None))
+  else if can_sum ~digits_of:n ~target:sqrt
+  then Some (sqrt, n)
+  else None
 ;;
 
 let t ~max_sqrt =
@@ -73,19 +42,17 @@ let t ~max_sqrt =
 
 let%expect_test "T(10^4)" =
   let s_numbers = t ~max_sqrt:100 in
-  Sequence.sum (module Int) s_numbers ~f:(fun (_, n, _) -> n)
-  |> [%sexp_of: int]
-  |> print_s;
+  Sequence.sum (module Int) s_numbers ~f:(fun (_, n) -> n) |> [%sexp_of: int] |> print_s;
   [%expect {| 41333 |}]
 ;;
 
 (* 128088830547982
-   25.844323874s *)
+   1.17600172s *)
 let main () =
   let max_sqrt = 1_000_000 in
   let s_numbers = t ~max_sqrt in
   s_numbers
-  |> Sequence.sum (module Int) ~f:(fun (_, n, _) -> n)
+  |> Sequence.sum (module Int) ~f:(fun (_, n) -> n)
   |> [%sexp_of: int]
   |> print_s
 ;;

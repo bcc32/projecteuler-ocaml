647a1db1caaf919a1c506c213a2e7948ac3ea63c
Aaron L. Zeng <me@bcc32.com>
2021-11-24 11:45:05 -0500

    Update ocamlformat

--- bench/src/bench_pow.ml
+++ bench/src/bench_pow.ml
@@ -5,9 +5,9 @@ let%bench_module "integer exponentiation" =
   (module struct
     let exponents = [ 0; 1; 2; 4; 5; 10; 15; 20; 25; 30 ]
 
-    let%bench ("Int.pow"[@indexed exp = exponents]) = Int.pow 3 exp
+    let%bench ("Int.pow" [@indexed exp = exponents]) = Int.pow 3 exp
 
-    let%bench ("pow_fast without functor"[@indexed exp = exponents]) =
+    let%bench ("pow_fast without functor" [@indexed exp = exponents]) =
       Number_theory.Int.addition_chain_pow 3 exp
     ;;
   end)

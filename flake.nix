9c146017bcdb7f7e596cab55583b41d2c7eeb3a3
Aaron L. Zeng <me@bcc32.com>
2026-05-02 00:08:26 -0400

    Use flake-utils from registry

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     ocaml-overlays.url = "github:nix-ocaml/nix-overlays";
     ocaml-overlays.inputs.nixpkgs.follows = "nixpkgs";
   };

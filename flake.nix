849a7310c7024cd5d77bd6d09bbf658a75a671b8
Aaron L. Zeng <me@bcc32.com>
2026-09-19 02:05:33 -0400

    Remove reference to deprecated stdenv.isLinux attribute

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       rec {
         devShells.default = mkShell {
           inputsFrom = [ packages.default ];
-          buildInputs = lib.optional stdenv.isLinux inotify-tools ++ [
+          buildInputs = lib.optional stdenv.hostPlatform.isLinux inotify-tools ++ [
             ocamlPackages.merlin
             ocamlformat
             ocamlPackages.ocaml-lsp
